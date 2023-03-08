0eda404a5b276c77809b3ae677b0d5702a786b45
Zhuo Chen <zhuoc@moqi.ai>
2023-03-07 18:39:40 -0600

    try to run this

--- labs/triangle_counting/template.cu
+++ labs/triangle_counting/template.cu
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <stdio.h>
+#include <math.h>
 
 #include "template.hu"
 
@@ -10,6 +11,38 @@ __global__ static void kernel_tc(uint64_t *__restrict__ triangleCounts, //!< per
                                  const uint32_t *const rowPtr,          //!< source node offsets in edgeDst
                                  const size_t numEdges                  //!< how many edges to count triangles for
 ) {
+    int x = blockDim.x * blockIdx.x + threadIdx.x; // x is per grid
+    for (int start = 0; start < numEdges; start += gridDim.x * blockDim.x) {
+        uint32_t U = edgeSrc[start+x];
+        uint32_t V = edgeDst[start+x];
+        uint32_t start_u = rowPtr[U];
+        uint32_t start_v = rowPtr[V];
+        uint32_t total_set_size = (rowPtr[U+1]-start_u)+(rowPtr[V+1]-start_v)-2;
+        uint64_t temporal_sum = 0;
+        start_u++;
+        start_v++;
+        uint32_t advance_u = edgeDst[start_u];
+        uint32_t advance_v = edgeDst[start_v];
+        while(total_set_size>1){
+            if(advance_u>advance_v){
+                start_v++;
+                advance_v = edgeDst[start_v];
+                total_set_size--;
+            }else if(advance_u<advance_v){
+                start_u++;
+                advance_u = edgeDst[start_u];
+                total_set_size--;
+            }else{
+                temporal_sum++;
+                start_v++;
+                advance_v = edgeDst[start_v];
+                start_u++;
+                advance_u = edgeDst[start_u];
+                total_set_size-=2;
+            }
+        }
+        triangleCounts[start+x]=temporal_sum;
+    }
 
   // Determine the source and destination node for the edge
 
@@ -22,17 +55,23 @@ uint64_t count_triangles(const pangolin::COOView<uint32_t> view, const int mode)
   //@@ create a pangolin::Vector (uint64_t) to hold per-edge triangle counts
   // Pangolin is backed by CUDA so you do not need to explicitly copy data between host and device.
   // You may find pangolin::Vector::data() function useful to get a pointer for your kernel to use.
-
-  uint64_t total = 0;
+  uint64_t result = 0;
+  uint64_t total = view.nnz();
+  std::vector<uint64_t> triangles(total);
+  uint64_t * cuda_mem;
 
   dim3 dimBlock(512);
   //@@ calculate the number of blocks needed
-  // dim3 dimGrid (ceil(number of non-zeros / dimBlock.x))
-
+  dim3 dimGrid (math.ceil(total/512f));
   if (mode == 1) {
-
     //@@ launch the linear search kernel here
-    // kernel_tc<<<dimGrid, dimBlock>>>(...)
+      CUDA_RUNTIME(cudaMalloc((void **)&cuda_mem,
+                            total * sizeof(uint64_t)));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
+      kernel_tc<<<dimGrid, dimBlock>>>(cuda_mem,view.row_ind(),view.col_ind(),view.row_ptr(),total);
+      CUDA_RUNTIME(cudaDeviceSynchronize());
+      CUDA_RUNTIME(cudaMemcpy(triangles.data(), cuda_mem,
+                              sizeof(uint64_t), cudaMemcpyDeviceToHost));
 
   } else if (2 == mode) {
 
@@ -45,5 +84,9 @@ uint64_t count_triangles(const pangolin::COOView<uint32_t> view, const int mode)
   }
 
   //@@ do a global reduction (on CPU or GPU) to produce the final triangle count
-  return total;
+
+  for(auto edge:triangles){
+      result+=edge;
+  }
+  return result;
 }
